4e1d7ac11421cca5248bc99d8699e142c14f9fdf
Freeman <swissarthurfreeman@gmail.com>
2020-09-15 13:19:20 +0200

    Generation of Balls and Class added

---
+++ balls.js
@@ -0,0 +1,7 @@
+class TBall {
+    constructor(x, y) {
+        this.r = [x, y];
+        this.v = [0, 0];
+    }
+}
+
--- main.js
+++ main.js
@@ -1,10 +1,26 @@
+function randPosition() {
+    function getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+    return [getRandomArbitrary(0,800), getRandomArbitrary(0,600)];
+}
+
 //Setup est appelé qu'une seule fois. 
+let balls = [];
 function setup() {
     createCanvas(800,600)
+    background(220)
+    
+    for (let i = 0; i < 10; i++) {
+        balls.push(new TBall( (randPosition())[0], (randPosition())[1] ) )
+    }
 }
+
 
 //added comment
 //Draw est appelé non-stop.
 function draw() {
-    background(220)
+    for (let i = 0; i <10; i++) {
+        ellipse( (balls[i].r)[0], (balls[i].r)[1], 20, 20)
+    }
 }
